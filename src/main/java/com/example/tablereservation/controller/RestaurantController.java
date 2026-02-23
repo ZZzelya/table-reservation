ce8731acaac2e45e1e7bcf94974dfd24ea3ba4b1
ZZzelya <zelenaleksandr3@gmail.com>
2026-02-23 21:01:01 +0300

    fourth commit

--- src/main/java/com/example/tablereservation/controller/RestaurantController.java
+++ src/main/java/com/example/tablereservation/controller/RestaurantController.java
@@ -8,12 +8,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/restaurants")
 @RequiredArgsConstructor
 public class RestaurantController {
 
@@ -22,7 +24,7 @@ public class RestaurantController {
     // GET все рестораны или с фильтром
     @GetMapping
     public List<RestaurantDto> getRestaurants(
-            @RequestParam(value = "cuisine", required = false) String cuisineType) {
+        @RequestParam(value = "cuisine", required = false) String cuisineType) {
         if (cuisineType != null && !cuisineType.isEmpty()) {
             return restaurantService.getRestaurantsByCuisine(cuisineType);
         } else {
